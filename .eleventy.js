fef54ce4fc6b270920b001b932ffea9840d6eb5a
Finn Nils Gedrath <finn.gedrath@gmail.com>
2022-03-22 10:57:33 +0100

    imagery test

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const fs = require('fs');
 
 const markdownIt = require('markdown-it');
 const markdownItAnchor = require('markdown-it-anchor');
@@ -27,37 +28,43 @@ module.exports = (eleventyConfig) => {
   // @see https://dev.to/22mahmoud/how-to-optimize-and-lazyload-images-on-eleventy-11ty-206h
   // eleventyConfig.addPassthroughCopy("_content/**/*.(jp?(e)g|png|webp|tiff|avif|svg)");
 
-  eleventyConfig.addNunjucksAsyncShortcode("Image", async (src, alt) => {
+  eleventyConfig.addNunjucksAsyncShortcode("Image", async function (src, alt, _sizes = "", cssClass = "") {
     if (!alt) {
       throw new Error(`Missing \`alt\` on myImage from: ${src}`);
     }
 
-    let stats = await elImage(src, {
+    let sizes = '';
+
+    if (_sizes === "") {
+      sizes = "(min-width: 1024px) 1024px, 100vw";
+    }
+
+    const inputPath = this.page.inputPath;
+    const url = this.page.url;
+
+    let stats = await elImage(`${path.dirname(inputPath)}/${src}`, {
       widths: [25, 320, 640, 960, 1200, 1800, 2400],
       formats: ["jpeg", "webp", "avif"],
-      urlPath: "",
-      outputDir: "./dist",
-      filenameFormat: function (id, src, width, format, options) {
-        // id: hash of the original image
-        // src: original image path
-        // width: current width in px
-        // format: current file format
-        // options: set of options passed to the Image call
-
+      urlPath: `/media`,
+      outputDir: `./dist/media`,
+      hashLength: 32,
+      filenameFormat: function (id, src, width, format) {
         const extension = path.extname(src);
-        const name = path.basename(src, extension);
+        const basename = path.basename(src, extension);
 
-        return `${id}-${name}-${width}w.${format}`;
+        return `${basename}-${width}w.${id}.${format}`;
       }
     });
 
-    let lowestSrc = stats["jpeg"][0];
+    const lowestSrc = stats["jpeg"][0];
 
+
+    // TODO: no reduce
     const srcset = Object.keys(stats).reduce(
       (acc, format) => ({
         ...acc,
         [format]: stats[format].reduce(
-          (_acc, curr) => `${_acc} ${curr.srcset} ,`,
+          (_acc, curr) => `${_acc} ${curr.srcset},`,
           ""
         ),
       }),
@@ -71,12 +78,13 @@ module.exports = (eleventyConfig) => {
       loading="lazy"
       alt="${alt}"
       src="${lowestSrc.url}"
-      sizes='(min-width: 1024px) 1024px, 100vw'
+      sizes="${sizes}"
       srcset="${srcset["jpeg"]}"
       width="${lowestSrc.width}"
-      height="${lowestSrc.height}">`;
+      height="${lowestSrc.height}"
+      class="${cssClass}">`;
 
-    return `<div class="image-wrapper"><picture> ${sourceAvif} ${sourceWebp} ${img} </picture></div>`;
+    return `<picture> ${sourceAvif} ${sourceWebp} ${img} </picture>`;
   });
 
 
